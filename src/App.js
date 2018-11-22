9e614915304e633dec236cb369621ddfb3a4a96c
Mela S <msetijo@gmail.com>
2018-11-21 22:21:23 -0500

    saving inputs inside question field

--- src/App.js
+++ src/App.js
@@ -9,39 +9,62 @@ class Question extends Component {
   render(){
     return(
       <div>
-        <input type="text" placeholder="Question" />
-        <Answers />
+        <input type="text" placeholder="Question" value={this.props.value} onChange={this.props.onChange} onSubmit={this.props.seeSurvey}/>
       </div>
     );
     
   }
 }
+
 class Questions extends Component {
   constructor(props){
     super(props);
     this.addQuestion = this.addQuestion.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.seeSurvey = this.seeSurvey.bind(this);
+    this.counter = 0;
     this.state = {
-      queAnsCombo : []
+      value: [],
+      questionComponents : []
     }
   }
+  seeSurvey(){
+    console.log('A name was submitted: ' + this.state.value);
+  }
+  handleChange(event){
+    this.setState({value: event.target.value});
+  }
   addQuestion(){
-    const queAnsCombo = this.state.queAnsCombo.slice();
-    queAnsCombo.push(Question);
-    this.setState({ queAnsCombo: queAnsCombo });
+    const questionComponent = () => {
+      return <div>
+                <Question value={this.state.value[counter]} onChange={this.handleChange} onSubmit={this.seeSurvey} />
+             </div>
+      }
+    const newArray = this.state.questionComponents.concat(questionComponent);
+    this.setState({ questionComponents: newArray });
   }
   render(){
-    const queAnsCombo = this.state.queAnsCombo.map((Element, index) => {
+    if(this.state.questionComponents === []){
+      const questionComponent = () => {
+        return <div>
+                  <Question value={this.state.value} onChange={this.handleChange} onSubmit={this.seeSurvey} />
+               </div>
+        }
+      const newArray = this.state.questionComponents.concat(questionComponent);
+      this.setState({ questionComponents: newArray });
+    }
+    const questions = this.state.questionComponents.map((Element, index) => {
       return <Element key={ index } index={ index } />
     });
       return(
-        <div className="question">
-         <input type="text" placeholder="Question" />
-         <Answers />
-         <div className="inputs">
-          { queAnsCombo }
-         </div>
-         <p><a id="addQuestion" onClick={this.addQuestion} href="#">Add Question</a></p>
-         <a id="saveSurvey" onClick={this.saveSurvey} href="#">Preview Survey</a>
+        <div>
+          <form onSubmit={this.seeSurvey}>
+          <div className="inputs">
+            {questions}
+          </div>
+          <p><a id="addQuestion" onClick={this.addQuestion} href="#">Add Question</a></p>
+          <input id="saveSurvey" type="submit" value="Survey Preview" />
+         </form>
         </div>
       );
   }
